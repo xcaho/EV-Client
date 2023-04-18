3787b6b64a02d508d92df2b617e53bb1c32242dd
Szymon Mazurek <szymaz1@st.amu.edu.pl>
2023-04-19 01:52:11 +0200

    Added 'your-appointments' component

---
+++ src/app/recruiter/your-appointments/your-appointments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-your-appointments',
+  templateUrl: './your-appointments.component.html',
+  styleUrls: ['./your-appointments.component.scss']
+})
+export class YourAppointmentsComponent {
+
+}
