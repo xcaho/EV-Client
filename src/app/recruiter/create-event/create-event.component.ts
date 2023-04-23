396ec0222c96fb6ac69eadce414604ad660e17c9
Szymon Mazurek <szymaz1@st.amu.edu.pl>
2023-04-23 23:17:48 +0200

    Refactored create-event form

--- src/app/recruiter/create-event/create-event.component.ts
+++ src/app/recruiter/create-event/create-event.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
-import {NgForm} from "@angular/forms";
+import {Component} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, FormGroupDirective, Validators} from "@angular/forms";
 import {EventDto} from "../../common/mainpage/EventDto";
 import { EventService } from '../../event.service';
+
+interface Event {
+  name: string;
+  duration: string;
+  breakTime: string;
+  endDate: string;
+  maxUsers: boolean;
+  nickname: string;
+  email: string;
+  password: string;
+  showPassword: boolean;
+}
 
 @Component({
   selector: 'app-create-event',
@@ -11,35 +23,90 @@ import { EventService } from '../../event.service';
 })
 export class CreateEventComponent {
 
-  public errorsMapVar = {
-    name: '',
-    description: '',
-    startDate: '',
-    endDate: '',
-    maxUsers: '',
-    duration: '',
-    breakTime: ''
-  };
-  constructor(private eventService: EventService) { }
+  reactiveForm!: FormGroup;
+  event: Event;
 
-  onSubmit(f: NgForm) {
-    this.errorsMapVar = {
-      name: '',
-      description: '',
-      startDate: '',
-      endDate: '',
-      maxUsers: '',
-      duration: '',
-      breakTime: ''
-    };
+  private validateTime(control: AbstractControl): { [key: string]: any } | null {
+    const time = control.value;
+    if (!time) {
+      return null;
+    }
+    const [hours, minutes] = time.split(':').map(Number);
+    const timeInMinutes = hours * 60 + minutes;
+    if (timeInMinutes < 15) {
+      return {invalidTime: true};
+    }
+    return null;
+  }
 
+  constructor(private eventService: EventService) {
+    this.event = {} as Event;
+  }
+
+  ngOnInit(): void {
+    this.reactiveForm = new FormGroup({
+      name: new FormControl(this.event.name, [
+        Validators.required
+      ]),
+      duration: new FormControl(this.event.duration, [
+        Validators.required,
+        this.validateTime
+      ]),
+      breakTime: new FormControl(this.event.breakTime, [
+        Validators.required
+      ]),
+      endDate: new FormControl(this.event.endDate, [
+        Validators.required
+      ]),
+      maxUsers: new FormControl(this.event.maxUsers, [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(20),
+      ]),
+    });
+  }
+
+  get name() {
+    return this.reactiveForm.get('name')!;
+  }
+
+  get duration() {
+    return this.reactiveForm.get('duration')!;
+  }
+
+  get breakTime() {
+    return this.reactiveForm.get('breakTime')!;
+  }
+
+  get endDate() {
+    return this.reactiveForm.get('endDate')!;
+  }
+
+  get maxUsers() {
+    return this.reactiveForm.get('maxUsers')!;
+  }
+
+
+  public validate(form: FormGroupDirective): void {
+    this.serverValidation(form);
+    if (this.reactiveForm.invalid) {
+      for (const control of Object.keys(this.reactiveForm.controls)) {
+        this.reactiveForm.controls[control].markAsTouched();
+      }
+      return;
+    }
+
+    this.event = this.reactiveForm.value;
+  }
+
+  serverValidation(f: FormGroupDirective) {
     let formContent = f.value
     let newEvent = new EventDto(
       formContent.name,
       formContent.description,
-      formContent.startDate,
+      "6666-06-06",
       formContent.endDate,
-      15,
+      formContent.maxUsers,
       60,
       15)
 
@@ -48,7 +115,6 @@ export class CreateEventComponent {
         console.log("Successfully added: " + JSON.stringify(response));
       }, exception => {
         let errorsMap = exception.error.errorsMap;
-        this.errorsMapVar = errorsMap;
         console.log(errorsMap);
       }
     )
