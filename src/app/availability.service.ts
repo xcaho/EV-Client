a172deee855206bf1941b7a444b75e6d1cd47136
Maskuu <makslas@interia.pl>
2023-05-30 22:33:16 +0200

    Moved updating available hours to service

--- src/app/availability.service.ts
+++ src/app/availability.service.ts
@@ -52,4 +52,65 @@ export class AvailabilityService {
   clearTemporaryAvailabilities() {
     this.temporaryAvailabilities = undefined
   }
+
+  updateAvailableHours(hoursList: string[], hoursChoice: String, events: EventDto[], eventId: number ) {
+    const totalLength = events[eventId].surveyDuration + events[eventId].surveyBreakTime;
+    const totalLengthDate = new Date();
+    totalLengthDate.setHours(Math.floor(totalLength / 60), totalLength % 60);
+
+    const [choiceHourValue, choiceMinuteValue] = hoursChoice.split(':').map(part => parseInt(part, 10));
+
+    const lowerBound = new Date();
+    const totalChoiceMinutes = choiceMinuteValue + choiceHourValue * 60;
+    const minutesDif = totalChoiceMinutes - totalLength;
+    lowerBound.setHours(Math.floor(minutesDif / 60), minutesDif % 60)
+
+    const upperBound = new Date();
+    const totalSumMinutes = choiceMinuteValue + totalLengthDate.getMinutes()
+    upperBound.setHours(choiceHourValue + totalLengthDate.getHours() + Math.floor(totalSumMinutes/60) , totalSumMinutes % 60)
+
+    const updatedHoursList = hoursList.filter(hour => {
+      const [hourValue, minuteValue] = hour.split(':').map(part => parseInt(part, 10));
+      const currentTime = new Date();
+      currentTime.setHours(hourValue, minuteValue);
+      return currentTime <= lowerBound || currentTime >= upperBound;
+    });
+
+    const ranges: string[] = [];
+
+    let startHour = updatedHoursList[0];
+    let endHour = updatedHoursList[0];
+
+    for (let i = 1; i<updatedHoursList.length; i++){
+      const currentHour = updatedHoursList[i];
+      const diff = this.getHourDiff(endHour, currentHour);
+
+      if(diff == 30) {
+        endHour = currentHour;
+      } else {
+        if (startHour !== endHour) {
+          ranges.push(`${startHour}-${endHour}`);
+        } else {
+          ranges.push(startHour);
+        }
+        startHour = currentHour;
+        endHour = currentHour;
+      }
+    }
+
+    if (startHour !== endHour) {
+      ranges.push(`${startHour}-${endHour}`);
+    } else {
+      ranges.push(startHour);
+    }
+
+    return ranges;
+  }
+
+  getHourDiff(hour1: string, hour2: string): number {
+    const time1 = new Date(`2000-01-01T${hour1}`);
+    const time2 = new Date(`2000-01-01T${hour2}`);
+
+    return Math.abs(time2.getTime() - time1.getTime()) / (1000 * 60);
+  }
 }
