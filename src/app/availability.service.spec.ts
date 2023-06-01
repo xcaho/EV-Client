88216f14b3bc8748f551c0fb2b289bfbbc79f9b8
Maskuu <makslas@interia.pl>
2023-06-01 18:32:21 +0200

    Fixed availability service tests

--- src/app/availability.service.spec.ts
+++ src/app/availability.service.spec.ts
@@ -1,5 +1,6 @@
 import { AvailabilityService} from "./availability.service";
 import { HttpClient } from '@angular/common/http';
+import {EventDto} from "./common/mainpage/EventDto";
 
 describe('AvailabilityService', () => {
   let availabilityService: AvailabilityService;
@@ -13,14 +14,11 @@ describe('AvailabilityService', () => {
   it('should return correct ranges', () => {
     const hoursList = ['09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30'];
     const hoursChoice = '10:00';
-    const events = [
-      {id:0, name: 'test1', description: '', researchStartDate:'', researchEndDate:'', endDate:'', maxUsers:3, slotsTaken:5, surveyDuration: 60, surveyBreakTime: 30 },
-    ];
-    const eventId = 0;
+    const events = new EventDto('test1', 'test1', '', '', '', 3, 60, 30, 5);
 
     const expectedRanges = [ '11:30-12:30'];
 
-    const result = availabilityService.updateAvailableHours(hoursList, hoursChoice, events, eventId);
+    const result = availabilityService.updateAvailableHours(hoursList, hoursChoice, events);
 
     expect(result).toEqual(expectedRanges);
   });
@@ -28,12 +26,11 @@ describe('AvailabilityService', () => {
   it('should return single hour when no ranges can be created', () => {
     const hoursList = ['09:00', '09:30', '10:00'];
     const hoursChoice = '09:00';
-    const events = [{id:0, name: 'test1', description: '', researchStartDate:'', researchEndDate:'', endDate:'', maxUsers:3, slotsTaken:5, surveyDuration: 30, surveyBreakTime: 30 }];
-    const eventId = 0;
+    const events = new EventDto('test1', 'test1', '', '', '', 3, 30, 30, 5);
 
     const expectedRanges = ['10:00'];
 
-    const result = availabilityService.updateAvailableHours(hoursList, hoursChoice, events, eventId);
+    const result = availabilityService.updateAvailableHours(hoursList, hoursChoice, events);
 
     expect(result).toEqual(expectedRanges);
   });
