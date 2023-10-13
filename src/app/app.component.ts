211b2f7678a11d34a84f40aa6a79486347979ad0
Szymon Mazurek <szymaz1@st.amu.edu.pl>
2023-10-13 23:30:20 +0200

    Improvements and fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import axios from 'axios';
+import {Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,26 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'easyvisit';
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    axios.interceptors.response.use(
+      (response) => {
+        console.log(response)
+        return response;
+      },
+      (error) => {
+        if (error.response && error.response.status === 404) {
+          console.error('Resource not found:', error.response.data);
+          this.router.navigate(['404']);
+        } else {
+          console.error('An error occurred:', error);
+        }
+        return Promise.reject(error);
+      }
+    );
+  }
 }
 
