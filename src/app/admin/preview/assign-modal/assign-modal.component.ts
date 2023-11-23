7bcb5a3dd0a3e7ee0cf8738f1b86b7f3657b045c
Michał Nowak <michalkarol.nowak2001@gmail.com>
2023-11-23 16:13:47 +0100

    Implemented getAllUsers() for assign-modal

--- src/app/admin/preview/assign-modal/assign-modal.component.ts
+++ src/app/admin/preview/assign-modal/assign-modal.component.ts
@@ -1,6 +1,8 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import {NgbModal, NgbModalRef} from "@ng-bootstrap/ng-bootstrap";
+import { User } from 'src/app/shared/dtos/User';
 import {AlertService} from "../../../common/alerts/service/alert.service";
+import {AdminService} from "../../../shared/services/admin.service";
 
 @Component({
   selector: 'app-assign-modal',
@@ -10,20 +12,21 @@ import {AlertService} from "../../../common/alerts/service/alert.service";
 export class AssignModalComponent {
   @ViewChild('content') content: ElementRef | undefined;
   private modalRef: NgbModalRef = null!;
-  public users: string[] = [
-    'Paweł Gaweł',
-    'Szymon Mazi',
-    'Fabian Fabianowski',
-    'Mazi Szymon',
-    'Mateusz Mati',
-    'Gaweł Paweł'
-  ];
+  public users: User[] = []
   public appointmentHolder: string = 'Szymon Mazi';
   public displayedUsers: string[] = [];
   public numberOfShowedUsers: number = 4;
 
   constructor(private modalService: NgbModal,
-              private alertService: AlertService) {
+              private alertService: AlertService,
+              private adminService: AdminService) {
+  }
+
+  ngOnInit() {
+
+    this.adminService.getAllUsers().subscribe(users => {
+      this.users = users;
+    })
   }
 
   public open(content: any): void {
