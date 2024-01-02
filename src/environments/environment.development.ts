10bdc5157987590faa713afc4875dfd7c498b1e7
Szymon Mazurek <91895260+xcaho@users.noreply.github.com>
2024-01-02 20:21:17 +0100

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080',
-  selfUrl: 'http://localhost:4200'
+  production: true,
+  apiUrl: 'https://easy-visit-a531d1dc0dd3.herokuapp.com',
+  selfUrl: 'https://easyvisit.netlify.app'
 };
